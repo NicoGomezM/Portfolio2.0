0f6babf1f8e567c4c6477630fcfcc1dfe0f3842d
Nico Gomez <nicolas.gomez2101@alumnos.ubiobio.cl>
2025-03-22 00:20:05 -0300

    Try 900k

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+// import tailwind from '@astrojs/tailwind';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // integrations: [tailwind()],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@styles': '/src/styles'
+      }
+    }
   },
   site: 'https://NicoGomezM.github.io',
-  base: '/Porfolio2.0/',
+  base: '/Porfolio2.0/',    
 });
